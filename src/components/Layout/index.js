0b72f4765db4f2ad44d499565bdee0366fbb6ee6
Arafat-erasung <arafaterasung@gmail.com>
2022-04-03 22:39:56 +0200

    created a Sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
+import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello World</>
+    return (
+    <>
+        <Sidebar />
+    </>
+    )
 }
 
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,2 +1,24 @@
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
+import Logo from '../../assets/assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
+const Sidebar = () => 
+    <div className='nav-bar'>
+        <Link className='logo' to="/">
+            <img src= {Logo} alt="logo" />
+            <img className='sub-logo' src= {LogoSubtitle} alt="slobadan" />
+        </Link>
+        <nav>
+            <NavLink exact='true' activeclassname="active" to='/'>
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+
+
+    </div>
+
+
+export default Sidebar;
