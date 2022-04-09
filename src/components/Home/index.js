e8c302a96bcc861457d2f45c63333db1b0faa378
Arafat-erasung <arafaterasung@gmail.com>
2022-04-09 03:41:16 +0200

    added animation to headline text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,45 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/assets/images/logo-s.png';
+import Logo from '../Logo';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['l','o','b','o','d','a','n'];
+    const jobArray = ['w','e','b','','d','e','v','e','l','o','p','e','r'];
+
+    useEffect(() =>{
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm 
-                <img src={LogoTitle} alt="developer"/>
-                lobadan
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass}_12`}>i,</span>
                 <br/>
-                web developer
+                <span className={`${letterClass}_13`}>I</span>
+                <span className={`${letterClass}_14`}>'m</span>
+                <img src={LogoTitle} alt="developer"/>
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={nameArray} 
+                idx={15} />
+                <br/>
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={jobArray} 
+                idx={22} />
                 </h1>
                 <h2>Front End developer |JavaScript Expert | YouTuber | TechXpert</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-        
+            <Logo />
         </div>
 
     )
